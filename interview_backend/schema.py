c1b03622b80dc16191f0553153fec6b48753c449
Daming Yang <lion@aosc.io>
2020-03-11 08:39:39 +0800

    schema: add finish_interview, cache_models et al.

--- interview_backend/schema.py
+++ interview_backend/schema.py
@@ -2,8 +2,6 @@ import datetime
 import functools
 import random
 
-from django.core.cache import caches
-from django.core.exceptions import PermissionDenied
 from django.utils import timezone
 
 import graphene
@@ -11,11 +9,13 @@ from graphene_django import DjangoObjectType
 
 from django.db import transaction
 from django.contrib.auth.models import User
+from graphql import GraphQLError
+
 from .models import Company, Problem
 from .models import Interview, Task, Submission, TestCase, ProblemSet
+from .cache_models import CurrentInterview
 
-current_interviews = caches['current_interviews']
-submission_results = caches['submission_results']
+PermissionError = GraphQLError('permission denied')
 
 
 class CompanyType(DjangoObjectType):
@@ -46,7 +46,7 @@ class CreateCompany(graphene.Mutation):
 
     def mutate(self, info, name):
         if not info.context.user.is_staff:
-            raise PermissionDenied()
+            raise PermissionError
         company = Company(name=name)
         company.save()
         return CreateCompany(company=company)
@@ -62,7 +62,7 @@ class CreateProblem(graphene.Mutation):
 
     def mutate(self, info, title, text, code_template):
         if not info.context.user.is_staff:
-            raise PermissionDenied()
+            raise PermissionError
         problem = Problem.objects.get_or_create(title=title)[0]
         problem.text = text
         problem.code_template = code_template
@@ -90,7 +90,10 @@ class StartInterview(graphene.Mutation):
             raise NotImplementedError
 
         user = info.context.user
+        current_interview = CurrentInterview(user_id=user.id)
 
+        if current_interview.get() is not None:
+            raise GraphQLError('another interview is ongoing')
         problems = Problem.objects.all()
         problems = random.choices(problems, k=3)
         duration_total = functools.reduce(
@@ -106,22 +109,54 @@ class StartInterview(graphene.Mutation):
             task = Task(problem=problem, interview=interview)
             task.save()
 
-        current_interviews.set(user.id, interview.id)
+        CurrentInterview(user_id=user.id).set(interview, timeout=3600 * 24)
         return StartInterview(id=interview.id)
+
+
+class FinishInterview(graphene.Mutation):
+    id = graphene.ID()
+
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+        current_interview = CurrentInterview(user_id=user.id)
+        interview = current_interview.get()
+        if interview is None:
+            raise GraphQLError('no interview is ongoing')
+        interview.finished_time = timezone.now()
+        interview.save()
+
+        current_interview.unset()
+        return FinishInterview(id=interview.id)
 
 
 class Query(graphene.ObjectType):
     current_user = graphene.Field(UserType)
+    current_interview = graphene.Field(InterviewType)
+    interview = graphene.Field(InterviewType, id=graphene.ID())
+    my_interviews = graphene.List(InterviewType)
     all_users = graphene.List(UserType)
     all_companies = graphene.List(CompanyType)
     all_problems = graphene.List(ProblemType)
+    all_interviews = graphene.List(InterviewType)
 
     def resolve_current_user(self, info, **kwargs):
         return info.context.user
 
+    def resolve_current_interview(self, info, **kwargs):
+        user = info.context.user
+        current_interview = CurrentInterview(user_id=user.id)
+        return current_interview.get()
+
+    def resolve_interview(self, info, id):
+        user = info.context.user
+        interview = Interview.objects.get(id=id)
+        if interview.user != user:
+            return None
+        return interview
+
     def resolve_all_users(self, info, **kwargs):
         if not info.context.user.is_staff:
-            raise PermissionDenied
+            raise PermissionError
         return User.objects.all()
 
     def resolve_all_companies(self, info, **kwargs):
@@ -129,8 +164,13 @@ class Query(graphene.ObjectType):
 
     def resolve_all_problems(self, info, **kwargs):
         if not info.context.user.is_staff:
-            raise PermissionDenied
+            raise PermissionError
         return Problem.objects.all()
+
+    def resolve_all_interviews(self, info, **kwargs):
+        if not info.context.user.is_staff:
+            raise PermissionError
+        return Interview.objects.all()
 
 
 class Mutation(graphene.ObjectType):
@@ -138,6 +178,7 @@ class Mutation(graphene.ObjectType):
     create_problem = CreateProblem.Field()
     # assign_problem = AssignProblem.Field()
     start_interview = StartInterview.Field()
+    finish_interview = FinishInterview.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
